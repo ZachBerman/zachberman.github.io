b9cc535e01c6e730b6bbd60aec28a1d6cb64089b
Your Name <zberms@gmail.com>
2016-07-14 22:36:23 +0000

    Add jQuery for intro

--- js/master.js
+++ js/master.js
@@ -16,18 +16,23 @@ $(function() {
 });
 
 // Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
+// $(function() {
+//     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+//         $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+//     }).on("focus", ".floating-label-form-group", function() {
+//         $(this).addClass("floating-label-form-group-with-focus");
+//     }).on("blur", ".floating-label-form-group", function() {
+//         $(this).removeClass("floating-label-form-group-with-focus");
+//     });
+// });
 
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// FadeIn name on image div
+  $(function () {
+    $( ".name, .skills" ).fadeIn(5000)
+  });
